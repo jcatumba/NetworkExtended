642715de411cc58defaa3c1897fd14fb0c081a21
Jorge Ruiz <jorgerev90@hotmail.com>
2012-12-26 12:34:21 -0500

    .gitignore and python_include modified.

--- python_include.c
+++ python_include.c
@@ -1,25 +1,43 @@
+/*===================================================================
+ * NetworkExtended
+ * Copyright (C) 2012 Jorge Catumba Ruiz <jorgerev90@gmail.com>
+ * Universidad Nacional de Colombia, Colombia
+ * ------------------------------------------------------------------
+ * This software is licensed under the terms of the GPL v0.3 for
+ * more information about the license see the license.txt file.
+ * ================================================================*/
+
 #include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <assert.h>
 
 void main(int argc, char** argv)
 {
-    PyObject* main_module, * main_dict, * expression;
+    PyObject* nx_module, * nx_dict, * expression;
 
     //We execute a simple command in the Python interpreter...
     Py_Initialize();
-    PyRun_SimpleString("print(\"Hello\\nWorld\")");
+    //PyRun_SimpleString("print(\"Hello\\nWorld\")");
 
     //Reference to the main module
-    main_module = PyImport_AddModule("__main__");
+    //nx_module = PyImport_AddModule("networkx");
+    nx_module = PyImport_ImportModule("networkx");
     //Main's dictionary and its copy
-    main_dict = PyModule_GetDict(main_module);
-    if (PyDict_Check(main_dict)){
+    nx_dict = PyModule_GetDict(nx_module);
+    if (PyDict_Check(nx_dict)){
         printf("Its a Dict!\n");
     } else {
         printf("Its not a Dict!\n");
     }
     //Evaluation of functions
-    expression = PyDict_GetItemString(main_dict, "print");
-    PyObject_CallFunction(expression, "s", "Hello again\n");
+    //expression = PyDict_GetItemString(nx_dict, "print");
+    //PyObject_CallFunction(expression, "s", "Hello again\n");
+    //PyRun_SimpleString("import networkx");
+    PyRun_SimpleString("G = networkx.DiGraph()");
+    PyRun_SimpleString("print G.edges()");
     Py_Finalize();
 }
