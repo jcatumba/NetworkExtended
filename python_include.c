fc0e795539bf717131e00731425e44597a46024b
Jorge Ruiz <jorgerev90@hotmail.com>
2012-12-26 21:35:09 -0500

    NetworkX creating a graph with one node.

--- python_include.c
+++ python_include.c
@@ -17,27 +17,80 @@
 
 void main(int argc, char** argv)
 {
-    PyObject* nx_module, * nx_dict, * expression;
+    int i;
+    PyObject* nx_module, * pFunc, *pNXName, * pNodes, * pNodesTuple;
+    PyObject* pObject, * graphNodes, * output, * pInt, * pItem;
 
-    //We execute a simple command in the Python interpreter...
     Py_Initialize();
-    //PyRun_SimpleString("print(\"Hello\\nWorld\")");
+    pNXName = PyString_FromString("networkx");
+    nx_module = PyImport_Import(pNXName);
+    Py_DECREF(pNXName);
 
-    //Reference to the main module
-    //nx_module = PyImport_AddModule("networkx");
-    nx_module = PyImport_ImportModule("networkx");
-    //Main's dictionary and its copy
-    nx_dict = PyModule_GetDict(nx_module);
-    if (PyDict_Check(nx_dict)){
-        printf("Its a Dict!\n");
+    if (nx_module != NULL) {
+        // Obteniendo nombre de la función
+        pFunc = PyObject_GetAttrString(nx_module, "Graph");
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            /*pNodes = PyList_New(4);
+            for (i=0;i<4;i++){
+                PyObject* pTuple = PyTuple_New(2);
+                PyTuple_SetItem(pTuple, 0, PyInt_FromLong(i));
+                PyTuple_SetItem(pTuple, 1, PyInt_FromLong(i+1));
+                PyList_Insert(pNodes, i, PyInt_FromLong(i));
+                Py_DECREF(pTuple);
+            }
+            pNodesTuple = PyTuple_New(1);
+            PyTuple_SetItem(pNodesTuple, 0, pNodes);*/
+            pObject = PyObject_CallObject(pFunc, NULL);
+
+            if (pObject != NULL) {
+                printf("Object created succesfully.\n");
+                graphNodes = PyObject_GetAttrString(pObject, "add_node");
+                if (graphNodes && PyCallable_Check(graphNodes)) {
+                    /*pNodes = PyList_New(4);
+                    for (i=0;i<4;i++) {
+                        PyList_Insert(pNodes, i, PyInt_FromLong(i));
+                        pItem = PyList_GetItem(pNodes, i);
+                    }
+                    pNodesTuple = PyTuple_New(1);
+                    PyTuple_SetItem(pNodesTuple, 0, pNodes);
+                    if (PyTuple_Check(pNodesTuple)) printf("Its ok.\n");*/
+                    pNodesTuple = PyTuple_New(1);
+                    PyTuple_SetItem(pNodesTuple, 0, PyInt_FromLong(2));
+                    PyObject_CallObject(graphNodes, pNodesTuple);
+
+                    if (output != NULL) {
+                        //if (PyList_Check(output)) printf("There are %i nodes.\n", PyList_Size(output));
+                        printf("Graph created.\n");
+                    } else {
+                        Py_DECREF(pNodesTuple);
+                        Py_DECREF(pNodes);
+                        Py_DECREF(graphNodes);
+                        Py_DECREF(pObject);
+                        PyErr_Print();
+                        fprintf(stderr, "Call failed.\n");
+                    }
+                    //printf("Object is callable.\n");
+                }
+                Py_DECREF(pObject);
+            } else {
+                Py_DECREF(pFunc);
+                Py_DECREF(nx_module);
+                PyErr_Print();
+                fprintf(stderr, "Call failed.\n");
+            }
+        } else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find funcion \"%s\".\n", argv[2]);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(nx_module);
+
     } else {
-        printf("Its not a Dict!\n");
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\".\n", argv[1]);
     }
-    //Evaluation of functions
-    //expression = PyDict_GetItemString(nx_dict, "print");
-    //PyObject_CallFunction(expression, "s", "Hello again\n");
-    //PyRun_SimpleString("import networkx");
-    PyRun_SimpleString("G = networkx.DiGraph()");
-    PyRun_SimpleString("print G.edges()");
+
     Py_Finalize();
 }
