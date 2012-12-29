67c0ba33d93db3b879b86caefce248abf063d468
Jorge Ruiz <jorgerev90@hotmail.com>
2012-12-29 16:55:30 -0500

    Remove code not needed.

--- python_include.c
+++ python_include.c
@@ -28,40 +28,21 @@ void main(int argc, char** argv)
     Py_DECREF(pNXName);
 
     if (nx_module != NULL) {
-        // Obteniendo nombre de la función
+        // Obteniendo referencia a la función Graph de networkx
         pFunc = PyObject_GetAttrString(nx_module, "Graph");
 
         if (pFunc && PyCallable_Check(pFunc)) {
-            /*pNodes = PyList_New(4);
-            for (i=0;i<4;i++){
-                PyObject* pTuple = PyTuple_New(2);
-                PyTuple_SetItem(pTuple, 0, PyInt_FromLong(i));
-                PyTuple_SetItem(pTuple, 1, PyInt_FromLong(i+1));
-                PyList_Insert(pNodes, i, PyInt_FromLong(i));
-                Py_DECREF(pTuple);
-            }
-            pNodesTuple = PyTuple_New(1);
-            PyTuple_SetItem(pNodesTuple, 0, pNodes);*/
             pObject = PyObject_CallObject(pFunc, NULL);
 
             if (pObject != NULL) {
                 printf("Object created succesfully.\n");
                 graphNodes = PyObject_GetAttrString(pObject, "add_node");
                 if (graphNodes && PyCallable_Check(graphNodes)) {
-                    /*pNodes = PyList_New(4);
-                    for (i=0;i<4;i++) {
-                        PyList_Insert(pNodes, i, PyInt_FromLong(i));
-                        pItem = PyList_GetItem(pNodes, i);
-                    }
-                    pNodesTuple = PyTuple_New(1);
-                    PyTuple_SetItem(pNodesTuple, 0, pNodes);
-                    if (PyTuple_Check(pNodesTuple)) printf("Its ok.\n");*/
                     pNodesTuple = PyTuple_New(1);
                     PyTuple_SetItem(pNodesTuple, 0, PyInt_FromLong(2));
                     PyObject_CallObject(graphNodes, pNodesTuple);
 
                     if (output != NULL) {
-                        //if (PyList_Check(output)) printf("There are %i nodes.\n", PyList_Size(output));
                         printf("Graph created.\n");
                     } else {
                         Py_DECREF(pNodesTuple);
@@ -71,7 +52,6 @@ void main(int argc, char** argv)
                         PyErr_Print();
                         fprintf(stderr, "Call failed.\n");
                     }
-                    //printf("Object is callable.\n");
                 }
                 Py_DECREF(pObject);
             } else {
